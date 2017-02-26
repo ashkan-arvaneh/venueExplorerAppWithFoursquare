80f284f8fd056a566174604525f6e773a4932314
Ashkan Arvaneh <pulse.1995@gmail.com>
2017-02-26 09:57:01 +0000

    Add API caller service and name filter

---
+++ dist/js/placesExplorerService.js
@@ -0,0 +1,24 @@
+var requestParms = {
+    clientId: "AHI100G0OL3133L5U0HMHQ1P0LZMOGDZYDE2UQBFB2J2DYC5",
+    clientSecret: "UZJ3R2RSYKZ4DOUWSJJM0PKL3XYJRPOFCZL1OS5LMSG4JLVQ",
+    version: "20131230"
+}
+
+app.factory('placesExplorerService', function ($resource) {
+
+    var requestUri = 'https://api.foursquare.com/v2/venues/:action';
+
+    return $resource(requestUri,
+    {
+        action: 'explore',
+        client_id: requestParms.clientId,
+        client_secret: requestParms.clientSecret,
+        v: requestParms.version,
+        venuePhotos: '1',
+        callback: 'JSON_CALLBACK'
+    },
+    {
+        get: { method: 'JSONP' }
+    });
+
+});
